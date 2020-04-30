abed23242b36c37ddc6266387420f3a5cc02f49a
CleuJunior <cleu.junior@gmail.com>
2020-04-30 00:44:59 -0300

    Conexão com o Banco de dados Usando MYSQLI

---
+++ database/exemplo01.php
@@ -0,0 +1,28 @@
+<?php
+
+$conn = new mysqli("127.0.0.1", "root", "", "phpmysql");
+
+
+if($conn->connect_error){
+    echo "Erro: " .  $conn->connect_error;
+
+
+}
+
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (?, ?)");
+
+$stmt->bind_param("ss", $login, $pass);
+
+$login = "user";
+$pass = "12345";
+
+$stmt->execute();
+
+$login = "root";
+$pass = "!@#$";
+
+$stmt->execute();
+
+
+
+
---
+++ database/exemplo02.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn = new mysqli("127.0.0.1", "root", "", "phpmysql");
+
+if ($conn->connect_error) {
+    echo "Erro: " . $conn->connect_error;
+
+
+}
+
+$result = $conn->query("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+$data = array();
+
+while ($row = $result->fetch_assoc()) {
+    array_push($data, $row);
+
+
+}
+
+echo json_encode($data);
+
+
+
